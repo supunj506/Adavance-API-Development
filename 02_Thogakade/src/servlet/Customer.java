5ce32d0bea16728277a83ecbfc159e5ee605bd1d
supunj506 <supunj506@gmail.com>
2023-11-20 22:04:29 +0530

    02 thogakade customer save in database successfully😊 :-)

---
+++ 02_Thogakade/src/servlet/Customer.java
@@ -0,0 +1,77 @@
+/*
+ * Author  : supun@mj
+ * Project : Adavnce API Development
+ * Date    : 11/20/2023
+ * Time    : 9:31 PM
+ * For GDSE course of IJSE Institute.
+ */
+
+package servlet;
+
+import db.DBConnection;
+import dto.CustomerDTO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@WebServlet(urlPatterns = "/customer")
+public class Customer extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)  {
+        ArrayList<CustomerDTO> customerList =new ArrayList<>();
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from customer");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+
+                customerList.add(
+                        new CustomerDTO(
+                                resultSet.getString(1),
+                                resultSet.getString(2),
+                                resultSet.getString(3),
+                                resultSet.getDouble(4)
+                        )
+                );
+
+            }
+
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        double salary = Double.parseDouble(req.getParameter("salary"));
+
+
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement stm = connection.prepareStatement("insert into customer values (?,?,?,?)");
+            stm.setString(1,id);
+            stm.setString(2,name);
+            stm.setString(3,address);
+            stm.setDouble(4,salary);
+            int i = stm.executeUpdate();
+            if(i>0){
+                resp.getWriter().write("<h1>Successfully save Customer </h1>");
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
