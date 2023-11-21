8a971b7c2548746c263e235bc0cfa52c60b61b81
supunj506 <supunj506@gmail.com>
2023-11-21 14:40:48 +0530

    02 thogakade customer servlet Update !!! :-)

--- 02_Thogakade/src/servlet/Customer.java
+++ 02_Thogakade/src/servlet/Customer.java
@@ -9,7 +9,6 @@
 package servlet;
 
 import db.DBConnection;
-import dto.CustomerDTO;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,30 +18,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 @WebServlet(urlPatterns = "/customer")
 public class Customer extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        ArrayList<CustomerDTO> customerList = new ArrayList<>();
-        try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("select * from customer");
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-
-                customerList.add(new CustomerDTO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getDouble(4)));
-
-            }
-
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
