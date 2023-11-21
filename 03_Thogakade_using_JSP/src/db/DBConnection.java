cfeec1985558376d6dfbf2cc4fb091b881f30ecb
supunj506 <supunj506@gmail.com>
2023-11-21 14:39:11 +0530

    02 thogakade file structure chande $ add new Module !!! :-)

---
+++ 03_Thogakade_using_JSP/src/db/DBConnection.java
@@ -0,0 +1,37 @@
+/*
+ * Author  : supun@mj
+ * Project : Adavnce API Development
+ * Date    : 11/20/2023
+ * Time    : 9:13 PM
+ * For GDSE course of IJSE Institute.
+ */
+
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static DBConnection dbConnection;
+
+    private final Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/thogakadeaad", "root", "1234");
+    }
+
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        if (dbConnection == null) {
+            dbConnection = new DBConnection();
+        }
+        return dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
